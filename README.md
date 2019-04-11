723211a4b1344e16d53e74a108e088655ab915cc
Nelsonfrank <Nelsonfr78@outlook.com>
2019-04-11 07:28:38 +0300

    Connect UI and Database and send data to Database"

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MemberAccess
-Show and display the list of member to everyone access and registered in the system.
+This is the web application containing of information about Company member(i.e, Firstname, Middlename, Lastname, title, genderof the member, Email address, phone number, occupation, house number of every member of the system )
+#User(member) of the system are able to use all member sign in, deleting the member from the application, update the application...
+
+#User can create his profile in the begginning before sign up then using some credetial from table to Sign up...
+
+
+#Database and user interface
+  *Tasks
+     -Insert data to the database
+        -- Connect database and form using php...
+        -- Handle any exception from your connection ..
+        **Php-Mysql Server Configuration..
+           Enable php-mysql extension (extension=mysqli.so) in /etc/php/7.x.x/php.ini
+           Restart Mysql server..
+           
--- form.php
+++ form.php
@@ -14,18 +14,24 @@
  $occupation =$_POST['occupation'];
  $houseno = $_POST['houseno'];
 
-$conn=new mysqli("localhost","root","","company");
+$conn=new mysqli("localhost","root","code1909->22","Company");
 
 if($conn->connect_error){
  echo "error" . $conn->connect_error;
 }
 
 
- $sql = "INSERT INTO member01 (firstname, middlename, lastname, title, gender, emailaddr, phoneno, occupation, houseno) VALUES  ('$firstname', '$middlename', '$lastname', '$title', '$gender', '$email', '$phoneno', '$occupation', '$houseno' )";
+ $sql = "INSERT INTO Member (FirstName, MiddleName, LastName, Title, Gender, Email, PhoneNo, Occupation, HouseNo) VALUES  ('$firstname', '$middlename', '$lastname', '$title', '$gender', '$email', '$phoneno', '$occupation', '$houseno' )";
 
- mysqli_query($conn,$sql);
+//  mysqli_query($conn,$sql);
 
-
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+}else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+ echo '<br/>';
+echo "your information are successfully inserted in Database 😁 ";
 
 
 ?>
